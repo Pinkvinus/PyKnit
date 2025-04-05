cd1a61847077c30be22170cdcc8c816d7cda9174
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-06 00:09:23 +0200

    changed naming convention

--- Test/unit/test_Instruction.py
+++ Test/unit/test_Instruction.py
@@ -9,13 +9,13 @@ sys.path.insert(0, project_root)  # Add the root directory to sys.path
 from src.Model.Knitting import Instruction as ins
 
 
-def test_instruction_initialisation_positive_values():
+def test_instruction_init_positive_values():
     test_instruction = ins.ShapingInstruction(1,2,3,4,"")
     assert test_instruction.num_rows == 1
     assert test_instruction.start_stitches == 2
     assert test_instruction.end_stitches == 3
 
-def test_instruction_inisialisation_negative_values():
+def test_instruction_init_negative_values():
     with pytest.raises(ValueError) as exc_info:
         ins.ShapingInstruction(-1,1,1,1,"")
     assert str(exc_info.value) == "Instruction can't be initialised with negative values"
@@ -25,3 +25,11 @@ def test_instruction_inisialisation_negative_values():
     with pytest.raises(ValueError) as exc_info:
         ins.ShapingInstruction(1,1,-1,1,"")
     assert str(exc_info.value) == "Instruction can't be initialised with negative values"
+
+def test_ShapingInstruction_init_positive_values():
+    test_instruction = ins.ShapingInstruction(1,2,3,4,"")
+    assert test_instruction.order == 4
+    
+def test_ShapingInstruction_str():
+    test_instruction = ins.ShapingInstruction(1,2,3,4,"None")
+    assert str(test_instruction) == "Number of rows: 1, initial number of stitches: 2, Terminal number of stitches: 3 Order: 4, Advanced instructions: None"
