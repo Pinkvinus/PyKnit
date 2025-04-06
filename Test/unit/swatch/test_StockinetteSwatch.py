1c992ba281982769ce28f30190917b28c8b64347
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-06 14:13:55 +0200

    Added tests

--- Test/unit/swatch/test_StockinetteSwatch.py
+++ Test/unit/swatch/test_StockinetteSwatch.py
@@ -10,7 +10,35 @@ print(sys.path)
 
 from src.Model.Knitting.Swatches.StockinetteSwatch import StockinetteSwatch
 
+def test_init_yarnid_non_positive_value():
+    with pytest.raises(ValueError) as exc_info:
+        StockinetteSwatch(-1,"3 mm", 1,1,1,1)
+    assert str(exc_info.value) == "Swatch: yarnid has to be a non-negative number"
 
-def test_StockinetteSwatch_get_stitches_per_cm_():
+def test_init_stitches_non_positive_value():
+    with pytest.raises(ValueError) as exc_info:
+        StockinetteSwatch(1,"3 mm", -1,1,1,1)
+    assert str(exc_info.value) == "Swatch: Stitches has to be a positive number"
+
+def test_init_rows_non_positive_value():
+    with pytest.raises(ValueError) as exc_info:
+        StockinetteSwatch(1,"3 mm", 1,-1,1,1)
+    assert str(exc_info.value) == "Swatch: Rows has to be a positive number"
+
+def test_init_width_non_positive_value():
+    with pytest.raises(ValueError) as exc_info:
+        StockinetteSwatch(1,"3 mm", 1,1,-1,1)
+    assert str(exc_info.value) == "Swatch: Width has to be a positive number"
+
+def test_init_height_non_positive_value():
+    with pytest.raises(ValueError) as exc_info:
+        StockinetteSwatch(1,"3 mm", 1,1,1,-1)
+    assert str(exc_info.value) == "Swatch: Height has to be a positive number"
+
+def test_get_stitches_per_cm_():
     swatch = StockinetteSwatch(1, "3 mm", 1, 1, 1, 1)
     assert swatch.get_stitches_per_cm() == 1
+
+def test_get_rows_per_cm_():
+    swatch = StockinetteSwatch(1, "3 mm", 1, 1, 1, 1)
+    assert swatch.get_rows_per_cm() == 1
