dcc627014afe2c50e3c0373e22dfdf7528b9eb0c
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-07 12:04:56 +0200

    added underbust shaping

--- src/Model/Knitting/Patterns/CropTop.py
+++ src/Model/Knitting/Patterns/CropTop.py
@@ -2,25 +2,28 @@ from src.Model.Knitting.Patterns.Pattern import Pattern
 from src.Model.Knitting.Instructions.CastOnInstruction import CastOnInstruction
 from src.Model.Knitting.Instructions.RibbingInstruction import RibbingInstruction
 from src.Model.Knitting.Instructions.ShapingInstruction import ShapingInstruction
+from src.Model.Knitting.Converters.MeasurementConverter import MeasurementConverter
 
 class CropTop(Pattern):
     def __init__(self, swatch, measurements):
         super().__init__(swatch, measurements)
         self.ribbing = 3
+        self.converter = MeasurementConverter(swatch, measurements)
     
     def get_instructions(self):
-        waist = self.measurements["waist"]
-        waist_stitches = waist * self.swatch.get_stitches_per_cm
+        waist_stitches = self.converter.get_stitches('waist')
 
         caston = CastOnInstruction(waist_stitches).get_instructions()
 
-        ribbing_rows = self.swatch.get_rows_per_cm() * self.ribbing
+        ribbing_rows = self.converter.get_rows(3)
         ribbing = RibbingInstruction(ribbing_rows, caston[1]).get_instructions()
 
-        underbust = 
-        waist_to_underbust = self.measurements["waist to underbust"]
-        waist_to_underbust_rows = self.swatch.get_rows_per_cm() * waist_to_underbust
-        shaping_underbust = ShapingInstruction()
+        underbust = self.converter.get_stitches('underbust')
+        waist_to_underbust_rows = self.converter.get_rows('waist to underbust')
+        ins = "try to fit the shaping stitches on the same 3rd of the garment"
+        shaping_underbust = ShapingInstruction(waist_to_underbust_rows, ribbing[1], underbust, 2, ins)
+
+        
 
 
 
