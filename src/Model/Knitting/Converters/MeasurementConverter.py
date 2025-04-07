38232912b83c859abfa12543c5b3601f09f67093
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-07 11:51:15 +0200

    added measurement Converter

---
+++ src/Model/Knitting/Converters/MeasurementConverter.py
@@ -0,0 +1,36 @@
+from src.Model.Measurements.Measurement import Measurement
+from src.Model.Knitting.Swatches.Swatch import Swatch
+
+class MeasurementConverter():
+    def __init__(self, swatch:Swatch, measurement:Measurement):
+        self.stitches_per_cm = swatch.get_stitches_per_cm()
+        self.rows_pr_cm = swatch.get_rows_per_cm()
+        self.measurement = measurement
+    
+    def get_stitches(self, m:str) -> int:
+        """
+            Converts a measurement into a number of stitches
+
+            params:
+
+                - m : measurement
+            
+            returns:
+
+                - The number of stitches that will create the same dimensions
+        """
+        return round(self.stitches_per_cm * self.measurement[m])
+    
+    def get_rows(self, m:str) -> int:
+        """
+            Converts a measurement into a number of rows
+
+            params:
+
+                - m : measurement
+
+            returns:
+                - The number of rows that will create the same dimensions
+        """
+        return round(self.rows_pr_cm * self.measurement[m])
+        
